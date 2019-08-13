d7f43cf18ed5b8fc0dc517ad439f0f6304b3cbc8
Kyler Barber <kylerbarber820@yahoo.com>
2019-08-13 22:46:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,31 +1,47 @@
+require 'pry'
+
 class CashRegister
-  attr_reader :total,:discount,:items
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
 
-  def add_item(item, price, qty = 1)
-    @total += price*qty
-    qty.times do
-      @items<<item
+@@items_a = []
+attr_accessor :total, :discount, :quantity, :price, :item
+
+      def initialize(discount = "0")
+
+        @total = 0
+        @discount = discount
+      end
+
+      def add_item(title, price, quantity = 1)
+
+        @quantity = quantity
+        @total += price * quantity.to_i
+        #binding.pry
+
+        def apply_discount
+          #applies the discount to the total price
+          #returns success message with updated total
+          #reduces the total
+          #returns a string error message that there is no discount to apply
+        if @discount != 0
+          self.total = (@total * ((100 - @discount)/100)).to_i
+                return "After the discount, the total comes to $#{@total}."
+          else
+               return "There is no discount to apply."
+          end
+       end
+
+        def items
+          #returns an array containing all items that have been added
+          @item = item
+          @@items_a << item
+          @@items_a
+          binding.pry
+        end
+
+        def void_last_transaction
+          #subtracts the last transaction from the total
+        end
+
+      end
+
     end
-    @last_transaction = price * qty
-  end
-
-  def apply_discount
-    if @discount > 0
-      deduction = (@discount*@total)/100
-      @total = @total - deduction
-      return "After the discount, the total comes to $#{@total}."
-    else
-      return "There is no discount to apply."
-    end
-  end
-
-  def void_last_transaction
-    item = @items.pop
-    @total = @total - (@last_transaction)
-  end
-end
