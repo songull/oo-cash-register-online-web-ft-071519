b368f82a7259d04b84bc155d5fa7097896c2eb27
Kyler Barber <kylerbarber820@yahoo.com>
2019-08-13 22:47:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,47 +1,36 @@
-require 'pry'
-
 class CashRegister
 
-@@items_a = []
-attr_accessor :total, :discount, :quantity, :price, :item
+  attr_accessor :total, :discount
 
-      def initialize(discount = "0")
 
-        @total = 0
-        @discount = discount
-      end
 
-      def add_item(title, price, quantity = 1)
+  def initialize(discount = 0)
+    @total = 0 
+    @discount =  discount
+    @items = []
+    @costs = []
+  end 
 
-        @quantity = quantity
-        @total += price * quantity.to_i
-        #binding.pry
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @items.concat([title]*quantity)
+    @costs.concat([price]*quantity)
+  end 
 
-        def apply_discount
-          #applies the discount to the total price
-          #returns success message with updated total
-          #reduces the total
-          #returns a string error message that there is no discount to apply
-        if @discount != 0
-          self.total = (@total * ((100 - @discount)/100)).to_i
-                return "After the discount, the total comes to $#{@total}."
-          else
-               return "There is no discount to apply."
-          end
-       end
+  def apply_discount
+    if @discount > 0 
+      @total = @total - (@discount/100.to_f) * @total
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else 
+      return "There is no discount to apply."
+    end 
+  end 
 
-        def items
-          #returns an array containing all items that have been added
-          @item = item
-          @@items_a << item
-          @@items_a
-          binding.pry
-        end
+  def items
+    @items
+  end 
 
-        def void_last_transaction
-          #subtracts the last transaction from the total
-        end
-
-      end
-
-    end
+  def void_last_transaction
+    @total = @total - @costs.last
+  end 
+end 
